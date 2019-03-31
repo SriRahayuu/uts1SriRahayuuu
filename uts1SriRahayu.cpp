7b81a81fa05909f16fc9334fe503412fdf86dd45
SriRahayuu <49122939+SriRahayuu@users.noreply.github.com>
2019-03-31 21:34:01 +0700

    Add files via upload

---
+++ uts1SriRahayu.cpp
@@ -0,0 +1,10 @@
+ #include <iostream>
+ using namespace std;
+ int main(){
+    cout <<"Nama : Sri Rahayu"<<endl;
+    cout <<"NIM  : 1829140017"<<endl;
+    cout <<"Kelas: TEKOM B"<<endl;
+    cout <<"Tahun: 2018"<<endl;
+
+    return 0;
+ }
